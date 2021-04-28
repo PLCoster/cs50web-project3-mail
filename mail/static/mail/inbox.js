73a52db7f168959fbd30d8e5d7634c8d2b08a924
PLCoster <58193305+PLCoster@users.noreply.github.com>
2021-04-27 23:23:04 -0700

    Added basic message flashing functionality for status, errors etc

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -13,11 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
     send_email()
   });
 
+  // Set up buttons to hide alert messages
+  document.querySelectorAll('.close').forEach((el) => {
+    console.log(el);
+    el.addEventListener('click', function() {this.parentElement.style.display = 'none'})
+  });
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
 
 function compose_email() {
+
+  hide_alerts();
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
@@ -31,6 +39,8 @@ function compose_email() {
 
 function load_mailbox(mailbox) {
 
+  hide_alerts();
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -41,13 +51,31 @@ function load_mailbox(mailbox) {
 
 function send_email() {
 
+  console.log("Trying to Send Email");
+
   // Get email details from form
   let recipients = document.querySelector('#compose-recipients').value;
   let subject = document.querySelector('#compose-subject').value;
   let body = document.querySelector('#compose-body').value;
 
-  // Check form for errors
-  console.log("Trying to Send Email");
+  // Check form for errors (no addresses)
+  if (recipients === '') {
+    flash_alert('warning', "Please add a valid recipient!")
+  }
+  console.log(recipients, subject, body);
 
   return false;
+}
+
+function flash_alert(type, text) {
+  // Flashes desired type of alert with specified text
+  hide_alerts();
+  let alert = document.querySelector(`.alert-${type}`)
+  alert.children[0].innerHTML = text;
+  alert.style.display = 'block';
+}
+
+function hide_alerts() {
+  // Hides all flashed alert messages
+  document.querySelectorAll('.alert').forEach((el) => el.style.display = 'none')
 }
