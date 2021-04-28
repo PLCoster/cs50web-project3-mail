b8ffa651925349d5bb32eda7e576fa9c9f6804fd
PLCoster <58193305+PLCoster@users.noreply.github.com>
2021-04-27 22:11:33 -0700

    Added submit event listener to compose-form

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,3 +1,4 @@
+// Setup pages and buttons when page is loaded
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
@@ -5,6 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // When compose_email form is submitted, send email
+  document.querySelector('#compose-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    send_email()
+  });
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -23,11 +30,24 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email() {
+
+  // Get email details from form
+  let recipients = document.querySelector('#compose-recipients').value;
+  let subject = document.querySelector('#compose-subject').value;
+  let body = document.querySelector('#compose-body').value;
+
+  // Check form for errors
+  console.log("Trying to Send Email");
+
+  return false;
 }
